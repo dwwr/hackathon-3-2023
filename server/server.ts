33f68588df2b49f77845720ffd647daba4cd5fa2
dwwr <derek.warnerreyes@olioapps.com>
2023-03-03 14:26:51 -0800

    WIP: generate AI music for sonic PI

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,5 @@
+const { Configuration, OpenAIApi } = require('openai')
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -5,26 +7,43 @@ const { exec } = require('child_process')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
-
 app.use(cors())
 
-app.post('/sendosc', (req, res) => {
-  // console.log('request', req.body)
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+app.post('/sendosc', async (req, res) => {
+  let completion
+  try {
+    completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt:
+        'Please write me a melody for sonic pi. The number of notes must be between 10 and 40 and may not under any circumstances exceed 40. The notes must be within the range 20 to 100 and may not under any circumstances exceed 100. Your response must not contain any words and must be in the form of an array of numbers satisfying the stated conditions.',
+      temperature: 0.5,
+      max_tokens: 1024,
+      n: 2,
+    })
+  } catch (error) {
+    return error
+  }
+  // console.log(completion?.data?.choices)
+  const args =
+    completion?.data?.choices[0]?.text + completion?.data?.choices[1]?.text
+  console.log(args)
   const ip = req.body.ip
   const port = req.body.port
   const address = req.body.address
   const typeTag = req.body.typeTag
-  const args = req.body.args
 
   const command = `sendosc ${ip} ${port} ${address} ${typeTag} "${args}"`
-  console.log('command', command)
-  exec(command, (error, stdout, stderr) => {
+  exec(command, error => {
     if (error) {
       console.error(`exec error: ${error}`)
       return
     }
-    console.log(`stdout: ${stdout}`)
-    console.error(`stderr: ${stderr}`)
   })
 
   res.sendStatus(200)
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import axios from 'axios'
 const App: React.FC = () => {
-  axios
-    .post(
-      'http://localhost:3000/sendosc',
-      {
-        ip: '192.168.86.30',
-        port: '4560',
-        address: '/melody',
-        typeTag: 's',
-        args: '60 62 64 65 67 69 71',
-      },
-      {
-        headers: {
-          'Access-Control-Allow-Origin': 'http://localhost:3000',
+  const handleSendMelody = () => {
+    axios
+      .post(
+        'http://localhost:3000/sendosc',
+        {
+          ip: '192.168.86.30',
+          port: '4560',
+          address: '/melody',
+          typeTag: 's',
+          args: '[60, 60, 67, 67, 69, 69, 67, 65, 64, 64]',
         },
-      }
-    )
-    .then(response => console.log(response))
-    .catch(error => console.error(error))
+        {
+          headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+          },
+        }
+      )
+      .then(response => console.log(response))
+      .catch(error => console.error(error))
+  }
 
-  return <div className="app">HELLO HACKATHON</div>
+  return (
+    <div className="app" onClick={() => handleSendMelody()}>
+      HELLO HACKATHON
+    </div>
+  )
 }
 export default App
