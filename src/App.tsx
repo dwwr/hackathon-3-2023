434dbce081faa4e18ff354af872e4e5b86ca5b4e
dwwr <derek.warnerreyes@olioapps.com>
2023-03-03 15:40:51 -0800

    add ztext for flair

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import axios from 'axios'
 import { MusicForm } from './Form'
+import { ZText } from './components/Ztext'
+import { Illustration } from 'react-zdog'
+import Zfont from 'zfont'
+import Zdog from 'zdog'
+import '../src/components/fonts'
+import NotoSans from './components/NotoSans-Regular.ttf'
+Zfont.init(Zdog)
+const font: Zdog.Font = new Zdog.Font({
+  src: NotoSans,
+})
 const App: React.FC = () => {
   const handleSendMelody = () => {
     axios
@@ -26,7 +36,26 @@ const App: React.FC = () => {
 
   return (
     <div className="app h-100">
-      <h1 onClick={() => handleSendMelody()}>HELLO HACKATHON</h1>
+      {/* <h1 onClick={() => handleSendMelody()}>HELLO HACKATHON</h1> */}
+      <div
+        style={{ height: 125, width: 300 }}
+        className="d-flex justify-self-center"
+      >
+        <Illustration
+          rotate={{ x: (Zdog.TAU * -15) / 360, y: (Zdog.TAU * 1) / 20 }}
+          element="canvas"
+          dragRotate={true}
+        >
+          <ZText
+            text="SlapGPT"
+            fontSize={40}
+            font={font}
+            stroke={4}
+            color="#80bfff"
+          />
+        </Illustration>
+      </div>
+
       <MusicForm />
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App'
+import Zdog from 'zdog'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 
-root.render(<App />)
+Zdog.waitForFonts().then(() => {
+  // Once the fonts are loaded, render app
+  root.render(<App />)
+})
